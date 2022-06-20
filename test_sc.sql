95defa37e0388cfb94aea54943b91cf35bde9b1f
xiaodaxia12138 <107560342+xiaodaxia12138@users.noreply.github.com>
2022-06-20 14:10:12 +0800

    1

--- test_sc.sql
+++ test_sc.sql
@@ -1,3 +1,19 @@
+drop table temp_hlw_operation_calss_special_privi_suchu;
+create table temp_hlw_operation_calss_special_privi_suchu
+(
+    cust_type varchar(30),
+    county_name varchar(10),
+    voulme_bl int,
+    voulme_bl_fk int,
+    priv_fee_0 decimal(10,2),
+    priv_fee_1 decimal(10,2),
+    arpu_avg decimal(10,5),
+    is_ct varchar(5),
+    ct decimal(5,5)
+) distributed by ('county_name');
+
+
+
 -- 输出表：
 select '全量特殊权限-合计',
         case when county_name in ('南岸','渝中') then '城一'
@@ -71,7 +87,7 @@ select '全量特殊权限-易携转重度','合计',sum(is_all) as a,sum(is_fk)
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_all =1 and p.is_yzx=1 
--- 3——阀值计算
+-- 3
 union all
 
 select '全量特殊权限-其他客户',
@@ -96,7 +112,7 @@ select '全量特殊权限-其他客户','合计',sum(is_all) as a,sum(is_fk) as
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_all =1 and p.is_qt=1 
---4
+-- 4
 union all
 
 select '1元包-合计',
@@ -121,7 +137,7 @@ select '1元包-合计','合计',sum(is_all) as a,sum(is_fk) as b ,
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuan=1
---5
+-- 5
 union all
 
 select '1元包-异网主卡',
@@ -146,7 +162,7 @@ select '1元包-异网主卡','合计',sum(is_all) as a,sum(is_fk) as b ,
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuan=1 and  p.is_ywzk=1
---6
+-- 6
 union all
 
 select '1元包-易携转重度客户',
@@ -171,7 +187,7 @@ select '1元包-易携转重度客户','合计',sum(is_all) as a,sum(is_fk) as b
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuan=1 and  p.is_yzx=1
---7
+-- 7
 union all
 
 select '1元包-其他客户',
@@ -196,7 +212,7 @@ select '1元包-其他客户','合计',sum(is_all) as a,sum(is_fk) as b ,
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuan=1 and  p.is_qt=1
---8
+-- 8
 union all
 
 select '1元包(合约版)-合计',
@@ -221,7 +237,7 @@ select '1元包(合约版)-合计','合计',sum(is_all) as a,sum(is_fk) as b ,
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuanheyue=1
---9
+-- 9
 union all
 
 select '1元包(合约版)-异网主卡客户',
@@ -246,7 +262,7 @@ select '1元包(合约版)-异网主卡客户','合计',sum(is_all) as a,sum(is_
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuanheyue=1 and p.is_ywzk=1
---10
+-- 10
 union all
 
 select '1元包(合约版)-易携转重度客户',
@@ -271,7 +287,7 @@ select '1元包(合约版)-易携转重度客户','合计',sum(is_all) as a,sum(
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuanheyue=1 and p.is_yzx=1
---11
+-- 11
 union all
 
 select '1元包(合约版)-其他客户',
@@ -296,7 +312,7 @@ select '1元包(合约版)-其他客户','合计',sum(is_all) as a,sum(is_fk) as
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_1yuanheyue=1 and p.is_qt=1
---12
+-- 12
 union all
 
 select '套餐折扣-合计',
@@ -321,7 +337,7 @@ select '套餐折扣-合计','合计',sum(is_all) as a,sum(is_fk) as b ,
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_tczk
---13
+-- 13
 union all
 
 select '套餐折扣-异网主卡客户',
@@ -346,7 +362,7 @@ select '套餐折扣-异网主卡客户','合计',sum(is_all) as a,sum(is_fk) as
         null as f,null as g
 from temp_hlw_operation_calss_special_privi_xiao p
 where p.is_tczk and p.is_ywzk=1
---14
+-- 14
 union all
 
 select '套餐折扣-易携转重度客户',
