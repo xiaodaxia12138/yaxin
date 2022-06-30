24c920ff969aea1397e246cc7d64e0a441631758
xiaodaxia12138 <107560342+xiaodaxia12138@users.noreply.github.com>
2022-06-30 11:01:13 +0800

    ok

--- test/testing.sql
+++ test/testing.sql
@@ -28,11 +28,11 @@ select  t1.user_id,t1.product_no,
         case when t7.user_id is not null then 1 else 0 end as is_18_68,
         case when t8.user_id is not null then 1 else 0 end as is_xy18,
         case when t9.product_no is not null then 1 else 0 end as is_fk 
-from cqbassdb.dw_product_outetype_dt_20211031 t1 
+from cqbassdb.dw_product_outetype_dt_20220531 t1 
 left join 
 (
     select distinct user_id
-    from cqbassdb.dw_user_useage_privilege_dt_20211031
+    from cqbassdb.dw_user_useage_privilege_dt_20220531
     where privsetid in (
     'gl_hkqcbtc','gl_hkqcbtk','gl_ydhk_19A_qyhy12','gl_ydhk_19A_qyhy6','gl_ydhk_19A_zkhy12','gl_ydhk_19A_zkhy6','gl_ydhk_19atc','gl_ydhk_19atk','gl_ydhk_19B_qyhy12',
     'gl_ydhk_19B_qyhy6','gl_ydhk_19B_zkhy12','gl_ydhk_19B_zkhy6','gl_ydhk_19btc','gl_ydhk_19btk','gl_ydhk_19C_qyhy12','gl_ydhk_19C_qyhy6','gl_ydhk_19C_zkhy12','gl_ydhk_19C_zkhy6',
@@ -40,12 +40,12 @@ left join
     'gl_ydhk_bzbhyb1','gl_ydwk19a','gl_ydwk39a','gl_ydwk59a','gl_ydwk19b','gl_ydwk39b','gl_ydwk59b','pip_main_qck19','pip_main_qck19n',
     'pip_main_qck39','pip_main_qck39n','pip_main_qck59','pip_main_qck59n','pip_main_jlk29','pip_main_jlk39','pip_main_5gdg59','pip_main_5gdg79','pip_main_5gdg99'
     ) 
-    and  nvl(enddate,'2050-01-01') >'2021-10-31' and enddate is null
+    and  nvl(enddate,'2050-01-01') >'2022-05-31' and enddate is null
 ) t2 on t1.user_id=t2.user_id
 left join 
 (
     select distinct user_id
-    from cqbassdb.dw_user_useage_privilege_dt_20211031
+    from cqbassdb.dw_user_useage_privilege_dt_20220531
     where privsetid in (
     'pip_main_qgbxl99','pip_main_supfmy68','gl_4gqgbxl98','pip_main_supfmy98','gl_xydjb_59b','pip_main_qgbxl169','gl_bdbxl_tcn','gl_xydjb_39b','gl_qgbxlfk','pip_main_qgbxl69',
     'pip_main_supfmy168','pip_main_supfmy128','gl_xydjb_59a','gl_xydjb_39a','gl_bdbxl_tc','pip_main_qgbxl159','pip_main_nolim109','pip_main_qgbxl128','pip_main_supfmy238','gl_4gqgbxl238',
@@ -56,47 +56,46 @@ left join
     'pip_gprs_xybdbxl3','gl_xyllbxl_b','pip_gprs_xsbdbxl1','pip_gprs_xsbdbxl4','gl_xyllbxl_a','pip_gprs_xybdbxl5','pip_qgbxl_58packfr','pip_qgbxl_88packfr','pip_gprs_xsbdbxl2',
     'pip_qgbxl_138packfr','pip_gprs_xsbdbxl3','pip_gprs_xybdbxl2','pip_gprs_xybdbxl1','gl_xyllbxl_e'
     ) 
-    and  nvl(enddate,'2050-01-01') >'2021-10-31' and enddate is null
+    and  nvl(enddate,'2050-01-01') >'2022-05-31' and enddate is null
 ) t2_1 on t1.user_id=t2_1.user_id
 left join
 (
     select distinct a.user_id,product_no
-    from cqbassdb.dw_user_imei_dt_20211031 a
+    from cqbassdb.dw_user_imei_dt_20220531 a
     inner join cqbassdb.dim_pub_5g_terminfo_tac b on a.tac=b.tac
 ) t3 on t1.user_id = t3.user_id                    -- 5G终端
-left join cqbassdb.dw_znjf_cust_zhudinet_info_outside_dm_20211031 t4 on t1.user_id=t4.user_id         -- 家庭用户
+left join cqbassdb.dw_znjf_cust_zhudinet_info_outside_dm_20220531 t4 on t1.user_id=t4.user_id         -- 家庭用户
 left join 
 (
     select distinct product_no 
     from 
     (
-        select  case when sum(totalbytes) >= 1040*10 then msisdn end as product_no,
-                sum(totalbytes) as totalbytes
+        select  case when sum(totalbytes) >= 1040*10 then msisdn end as product_no
         from cqbassdb.dw_gprs_app_bh_dt_20220531
         where subtypename in ('优酷','腾讯视频','爱奇艺视频')
         group by msisdn
         union all 
-        select msisdn as product_no,totalbytes
+        select msisdn as product_no
         from cqbassdb.dw_gprs_app_bh_dt_20220531
         where subtypename in ('优酷','腾讯视频','爱奇艺视频') and usedays >= 15
     ) a 
 ) t5 on t1.product_no=t5.product_no             -- 权益融合
 left join
 (
-    select user_id from cqbassdb.dw_user_nowuse_privilege_dt_20211031 where total_value >= 68
+    select user_id from cqbassdb.dw_user_nowuse_privilege_dt_20220531 where total_value >= 68
 ) t6 on t1.user_id=t6.user_id              -- 大于68
 left join
 (
-    select user_id from cqbassdb.dw_user_nowuse_privilege_dt_20211031 where total_value between 18 and 68 
+    select user_id from cqbassdb.dw_user_nowuse_privilege_dt_20220531 where total_value between 18 and 68 
 ) t7 on t1.user_id=t7.user_id     
 left join           -- 18-68
 (
-    select user_id,'1'  from cqbassdb.dw_user_nowuse_privilege_dt_20211031 where total_value <= 18
+    select user_id,'1'  from cqbassdb.dw_user_nowuse_privilege_dt_20220531 where total_value <= 18
 ) t8 on t1.user_id=t8.user_id               -- 小于18
 left join 
 (
     select distinct product_no
-    from cqbassdb.dw_zzqs_user_sxk_detail_dt_20211031 
+    from cqbassdb.dw_zzqs_user_sxk_detail_dt_20220531 
     where is_zk_priv='否'
 ) t9 on t1.product_no=t9.product_no
 where t1.active=1 and substr(t1.gra_product_attr_id1,5,1)=1
@@ -117,23 +116,23 @@ insert into temp_hlw_5g_tb_sale_map_info_02
 select t1.user_id,t1.product_no,
         t2.5gtb_cnt,t10.cnt,
         case when   t3.user_id is not null or
-                    t4.user_id is not null or
+                    t4.user_id is null or
                     t5.user_id is not null or              
                     t6.product_no is not null or
                     t7.user_id is not null or
                     t8.user_id is not null or
                     t9.user_id is not null then 0 else 1 end as is_yyx,
         case when t2.user_id is not null then 1 else 0 end as is_5gdd 
-from cqbassdb.dw_product_outetype_dt_20211031 t1
+from cqbassdb.dw_product_outetype_dt_20220531 t1
 left join 
 (
-    select user_id,count(1) as 5gtb_cnt from cqbassdb.dw_5g_privilege_user_dt_20211031 where typename in ('5G叠加包','5G套餐')
+    select user_id,count(1) as 5gtb_cnt from cqbassdb.dw_5g_privilege_user_dt_20220531 where typename in ('5G叠加包','5G套餐')
     group by user_id
 ) t2 on t1.user_id=t2.user_id
 left join 
 (  
     select distinct user_id
-    from  cqbassdb.dw_user_useage_privilege_dt_20211031
+    from  cqbassdb.dw_user_useage_privilege_dt_20220531
     where privsetid in ('gl_zq_vip_nodisturb','gl_zq_svip_nodisturb','gl_wh_nodisturb','gl_wapbxl_nodisturb','gl_tsmg_nodisturb','gl_qt_nodisturb','gl_qf_nodisturb'
     'gl_ctcxs4','gl_11yhg28','gl_1yhg2019_hl','gl_1yhg2019_t','gl_1yhgn2019_t','gl_1yhgn2019_b',
     'gl_1yhgn2019_a','gl_1yhgn2019_c','gl_1yhg2019_gn','gl_1yhg2019_g','gl_1yhg2019_f',
@@ -145,7 +144,7 @@ left join
     'gl_sharenew38_clzx','gl_zqxkh_10y','gl_zqxkh_20y','gl_zqxkh_30y','gl_zqxkh_40y','gl_zqxkh_50y','gl_zqxkh_60y','gl_zqxkh_70y','gl_zqxkh_80y','gl_zqxkhcy_10y',
     'gl_zqxkhcy_20y','gl_zqxkhcy_30y','gl_zqxkhcy_40y','gl_zqxkhcy_50y','gl_zqxkhcy_60y','gl_zqxkhcy_70y','gl_zqxkhcy_80y','pip_fee_qgbxl60','pip_fee_qgbxl60_2','gl_fptc3zg_2018','gl_4gbdxrw_60','gl_4gbdxrwhc_60'
     )
-    and nvl(enddate,'2050-01-01') >'2021-10-31' and enddate is null
+    and nvl(enddate,'2050-01-01') >'2022-05-31' and enddate is null
 ) t3 on t1.user_id=t3.user_id
 left join
 (
@@ -153,11 +152,11 @@ left join
     from 
     (
         select distinct a.user_id,a.product_no
-        from cqbassdb.dw_user_imei_dt_20211031 a
+        from cqbassdb.dw_user_imei_dt_20220531 a
         inner join cqbassdb.dim_pub_5g_terminfo_tac b on a.tac=b.tac
         union all
         select distinct a.user_id,a.product_no 
-        from cqbassdb.dw_user_imei_dt_20211031 a
+        from cqbassdb.dw_user_imei_dt_20220531 a
         left join cqbassdb.dim_pub_all_terminfo_tac b on a.tac=b.tac
         where  n09  in ('1','6','7','9') 
     ) a 
@@ -167,47 +166,55 @@ left join
     select distinct user_id 
     from 
     (
-        select distinct user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220624
+        select distinct user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220531
+        where  is_llgx=1 
         union  all
-        select distinct user_id1 as user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220624
+        select distinct user_id1 as user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220531
         where  is_llgx=1 
         union all
-        select distinct user_id2 as user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220624
+        select distinct user_id2 as user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220531
+        where  is_llgx=1 
+        union all
+        select distinct user_id3 as user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220531
+        where  is_llgx=1 
+        union all
+        select distinct user_id4 as user_id from cqbassdb.DW_ZZQS_USER_LLGX_DT_20220531
+        where  is_llgx=1 
     ) a 
 ) t5 on t1.user_id=t5.user_id
 left join 
 (
     select distinct product_no
-    from cqbassdb.dw_zzqs_user_conn_qianyue_dt_20211031
+    from cqbassdb.dw_zzqs_user_conn_qianyue_dt_20220531
     where age >= 60
 ) t6 on t1.product_no=t6.product_no
 left join 
 (
     select distinct user_id
-    from cqbassdb.dw_user_charges_useage_mm_202109
+    from cqbassdb.dw_user_charges_useage_mm_202205
     where usageratio <= 0.3 or dateratio <=0.3
 ) t7 on t1.user_id=t7.user_id
 left join 
 (
     select distinct user_id
-    from cqbassdb.dwb_imei_double_card_main_type_dt_20210831 
+    from cqbassdb.dwb_imei_double_card_main_type_dt_20220531 
     where product_no is not null and card_type in ('移动移动','电信移动','联通移动')
 ) t8 on t1.user_id=t8.user_id
 left join
 (
     select distinct user_id 
-    from cqbassdb.dw_zzqs_5g_tb_detail_dt_20211031
+    from cqbassdb.dw_zzqs_5g_tb_detail_dt_20220531
     where userstatus_name not in ('临时生成资料','临时资料交费开机','回退') 
-    and (enddate>='2021-11-01' or enddate is null) 
-    and operate_date between '2021-10-01' and '2021-10-31' 
+    and (enddate>='2022-06-01' or enddate is null) 
+    and operate_date between '2022-05-01' and '2022-05-31' 
     and priv_type not in ('副卡','升舱包')
 ) t9 on t1.user_id=t9.user_id
 left join
 (
-    select distinct user_id,count(1) as cnt from cqbassdb.dw_zzqs_5g_tb_detail_dt_20211031
+    select distinct user_id,count(1) as cnt from cqbassdb.dw_zzqs_5g_tb_detail_dt_20220531
     where userstatus_name not in ('临时生成资料','临时资料交费开机','回退')  
-    and operate_date between '2021-01-01' and '2021-10-31' 
-    and (enddate>='2021-11-01' or enddate is null) 
+    and operate_date between '2022-01-01' and '2022-05-31' 
+    and (enddate>='2022-06-01' or enddate is null) 
     and priv_type not in ('副卡','升舱包')
     and priv_type='5G叠加包'
     group by user_id
@@ -251,8 +258,8 @@ left join
 left join 
 (
     select user_id
-    from cqbassdb.dw_product_outetype_dt_20211031
-    where active=1 and open_date >= '2021-01-01'
+    from cqbassdb.dw_product_outetype_dt_20220531
+    where active=1 and open_date >= '2022-01-01'
 )t4 on t1.user_id=t4.user_id
 group by case when t1.is_xy=1 then 'xy' 
             when t1.is_xy=0 and t1.is_bxl=1 then 'bxl'
@@ -293,8 +300,8 @@ left join
 left join 
 (
     select user_id
-    from cqbassdb.dw_product_outetype_dt_20211031
-    where active=1 and open_date >= '2021-01-01'
+    from cqbassdb.dw_product_outetype_dt_20220531
+    where active=1 and open_date >= '2022-01-01'
 )t4 on t1.user_id=t4.user_id
 ;
 -- 插入副卡分类
@@ -324,8 +331,8 @@ left join
 left join 
 (
     select user_id
-    from cqbassdb.dw_product_outetype_dt_20211031
-    where active=1 and open_date >= '2021-01-01'
+    from cqbassdb.dw_product_outetype_dt_20220531
+    where active=1 and open_date >= '2022-01-01'
 )t4 on t1.user_id=t4.user_id
 where t1.is_fk=1
 group by case when t1.is_fk=1 then 'fk' end
